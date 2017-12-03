d39694432920e7423cfbedef73cb448b72d493ac
exel80 <theexel80@gmail.com>
2017-12-03 15:30:28 +0200

    Organising files

---
+++ OxidePlugins/Holdfast/testPlugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Oxide.Core;
+using Oxide.Core.Plugins;
+using Oxide.Core.Libraries.Covalence;
+using Newtonsoft.Json;
+using HoldfastGame;
+
+namespace Oxide.Plugins
+{
+    [Info("TestPlugin", "Exel80", "1.0.0")]
+    [Description("This is really simple and easy plugin.")]
+    class testPlugin : HoldfastPlugin
+    {
+        void Init()
+        {
+            Puts($"Test successful!");
+        }
+
+        private void OnRoundStart() => Puts("! ROUNDSTART !");
+
+        private void OnPlayerChat(RoundPlayer roundplayer, string message)
+        {
+            WeaponHolder w;
+            w.
+            PlayerBase p = roundplayer.PlayerBase;
+            Puts(p.Health.ToString() + " " + roundplayer.WeaponHolder.CanJump.ToString());
+            Puts(message);
+        }
+
+        private void OnAimCannon(UnityEngine.Vector2 rotationInput) => Puts("!!!");
+
+        [ChatCommand("test")]
+        void TestCommand(IPlayer player, string command, string[] args)
+        {
+            player.Reply("Test successful!");
+            Puts("Test successful!");
+        }
+    }
+}
